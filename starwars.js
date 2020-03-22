9dff86d677616364d9fd6ffcb2018e951eaee414
zlphillips <60951969+zlphillips@users.noreply.github.com>
2020-03-22 02:22:41 -0400

    first revision

--- starwars.js
+++ starwars.js
@@ -41,7 +41,11 @@ fetch('https://swapi.co/api/starships/')
                       if (s.name === 'Imperial shuttle'){
                         let listItem = document.createElement('ul');
                         listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
-                        imperialShuttle.appendChild(listItem);}       
+                        imperialShuttle.appendChild(listItem);}
+                        if (s.name === 'EF76 Nebulon-B escort frigate'){
+                          let listItem = document.createElement('ul');
+                          listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                          ef76NebulonBEscortFrigate.appendChild(listItem);}        
     }}
 );
 fetch('https://swapi.co/api/starships/?page=2')
@@ -52,9 +56,46 @@ fetch('https://swapi.co/api/starships/?page=2')
   console.log(json)
     let ships = json.results;
     for (s of ships){
+      if (s.name === 'Calamari Cruiser'){
         let listItem = document.createElement('ul');
         listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
-        starShipList2.appendChild(listItem);
+        calamariCruiser.appendChild(listItem);}
+        if (s.name === 'A-wing'){
+          let listItem = document.createElement('ul');
+          listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+          aWing.appendChild(listItem);}
+          if (s.name === 'B-wing'){
+            let listItem = document.createElement('ul');
+            listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+            bWing.appendChild(listItem);}
+            if (s.name === 'Republic Cruiser'){
+              let listItem = document.createElement('ul');
+              listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+              republicCruiser.appendChild(listItem);}
+              if (s.name === 'Naboo fighter'){
+                let listItem = document.createElement('ul');
+                listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                nabooFighter.appendChild(listItem);}
+                if (s.name === 'Naboo Royal Starship'){
+                  let listItem = document.createElement('ul');
+                  listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                  nabooRoyalStarship.appendChild(listItem);}
+                  if (s.name === 'Scimitar'){
+                    let listItem = document.createElement('ul');
+                    listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                    scimitar.appendChild(listItem);}
+                    if (s.name === 'J-type diplomatic barge'){
+                      let listItem = document.createElement('ul');
+                      listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                      jTypeDiplomaticBarge.appendChild(listItem);}
+                      if (s.name === 'AA-9 Coruscant freighter'){
+                        let listItem = document.createElement('ul');
+                        listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                        aa9CoruscantFreighter.appendChild(listItem);}
+                        if (s.name === 'Jedi starfighter'){
+                          let listItem = document.createElement('ul');
+                          listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                          jediStarfighter.appendChild(listItem);}
     }
 }
 );
@@ -66,10 +107,47 @@ fetch('https://swapi.co/api/starships/?page=3')
   console.log(json)
     let ships = json.results;
     for (s of ships){ 
+      if (s.name === 'H-type Nubian yacht'){
         let listItem = document.createElement('ul');
         listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
-        starShipList3.appendChild(listItem);
-    }
+        hTypeNubianYacht.appendChild(listItem);}
+        if (s.name === 'Star Destroyer'){
+          let listItem = document.createElement('ul');
+          listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+          starDestroyer.appendChild(listItem);}
+          if (s.name === 'Trade Federation cruiser'){
+            let listItem = document.createElement('ul');
+            listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+            tradeFederationCruiser.appendChild(listItem);}
+            if (s.name === 'Theta-class T-2c shuttle'){
+              let listItem = document.createElement('ul');
+              listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+              thetaClassT2CShuttle.appendChild(listItem);}
+              if (s.name === 'T-70 X-wing fighter'){
+                let listItem = document.createElement('ul');
+                listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                t70XWingFighter.appendChild(listItem);}
+                if (s.name === 'Rebel transport'){
+                  let listItem = document.createElement('ul');
+                  listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                  rebelTransport.appendChild(listItem);}
+                  if (s.name === 'Droid control ship'){
+                    let listItem = document.createElement('ul');
+                    listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                    droidControlShip.appendChild(listItem);}
+                    if (s.name === 'Republic Assault ship'){
+                      let listItem = document.createElement('ul');
+                      listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                      republicAssaultShip.appendChild(listItem);}
+                      if (s.name === 'Solar Sailer'){
+                        let listItem = document.createElement('ul');
+                        listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                        solarSailer.appendChild(listItem);}
+                        if (s.name === 'Republic attack cruiser'){
+                          let listItem = document.createElement('ul');
+                          listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                          republicAttackCruiser.appendChild(listItem);}
+                        }
 }
 );
 fetch('https://swapi.co/api/starships/?page=4')
@@ -80,9 +158,34 @@ fetch('https://swapi.co/api/starships/?page=4')
   console.log(json)
     let ships = json.results;
     for (s of ships){
+      if (s.name === 'Naboo star skiff'){
         let listItem = document.createElement('ul');
         listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
-        starShipList4.appendChild(listItem);  
+        nabooStarSkiff.appendChild(listItem);}
+        if (s.name === 'Jedi Interceptor'){
+          let listItem = document.createElement('ul');
+          listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+          jediInterceptor.appendChild(listItem);}
+          if (s.name === 'arc-170'){
+            let listItem = document.createElement('ul');
+            listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+            arc170.appendChild(listItem);}
+            if (s.name === 'Belbullab-22 starfighter'){
+              let listItem = document.createElement('ul');
+              listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+              belbullab22Starfighter.appendChild(listItem);}
+              if (s.name === 'V-wing'){
+                let listItem = document.createElement('ul');
+                listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                vWing.appendChild(listItem);}
+                if (s.name === 'CR90 corvette'){
+                  let listItem = document.createElement('ul');
+                  listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                  cr90Corvette.appendChild(listItem);}
+                  if (s.name === 'Banking clan frigate'){
+                    let listItem = document.createElement('ul');
+                    listItem.innerHTML = '<p>' + ' Ship Class: ' + s.starship_class + ' Number of Crew: '+ s.crew +' Number of Passengers: '+ s.passengers + '</p>';
+                    bankingClanFrigate.appendChild(listItem);}  
     }
 }
 );
